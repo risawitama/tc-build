04ddccbb0ffb2cd50ec59a14e03a546a70bc193b
Angga R <angga@linuxmail.org>
2023-01-26 13:46:35 +0700

    utils: upgrade binutisl to 2.40

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ def current_binutils():
     Simple getter for current stable binutils release
     :return: The current stable release of binutils
     """
-    return "binutils-2.39"
+    return "binutils-2.40"
 
 
 def download_binutils(folder):
@@ -69,7 +69,7 @@ def verify_binutils_checksum(file):
             if not data:
                 break
             file_hash.update(data)
-    good_hash = "68e038f339a8c21faa19a57bbc447a51c817f47c2e06d740847c6e9cc3396c025d35d5369fa8c3f8b70414757c89f0e577939ddc0d70f283182504920f53b0a3"
+    good_hash = "a37e042523bc46494d99d5637c3f3d8f9956d9477b748b3b1f6d7dfbb8d968ed52c932e88a4e946c6f77b8f48f1e1b360ca54c3d298f17193f3b4963472f6925"
     if file_hash.hexdigest() != good_hash:
         raise RuntimeError(
             "binutils: SHA512 checksum does not match known good one!")
